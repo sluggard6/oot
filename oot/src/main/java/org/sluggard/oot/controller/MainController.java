f42327b9eeae5b2f5e950b9fabf54f88f134b873
frank <sydra@163.com>
2020-06-29 01:20:39 +0800

    requestbody super big test

--- oot/src/main/java/org/sluggard/oot/controller/MainController.java
+++ oot/src/main/java/org/sluggard/oot/controller/MainController.java
@@ -2,6 +2,7 @@ package org.sluggard.oot.controller;
 
 import org.sluggard.oot.dao.SimpleDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,12 @@ public class MainController {
 	public String main() {
 		simpleDao.runSimpleSql();
 		return "a";
+	}
+	
+	@RequestMapping("test")
+	public String test(@RequestBody String data) {
+		System.out.println(data);
+		return data;
 	}
 
 }
